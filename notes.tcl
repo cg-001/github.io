798be51ee3c53442d0bcda32ebef052726148222
cg-001 <88622028+cg-001@users.noreply.github.com>
2021-08-23 21:20:21 +0800

    Add files via upload

--- notes.tcl
+++ notes.tcl
@@ -21,18 +21,21 @@ proc initial {} {
     set xsw [winfo screenwidth .]
     set xsh [winfo screenheight .]
     
-    set mwidth [expr int($xsw*0.8)]
-    set mheight [expr  int($xsh*0.8)]
+    set mwidth [expr int($xsw*0.7)]
+    set mheight [expr  int($xsh*0.7)]
     
-    wm maxsize . $mwidth $mheight
-    wm minsize .  500 300
-    wm geometry . +150+150  ;#窗口左上角在150.150处。
+
+    wm minsize .  500 120
+    wm geometry . ${mwidth}x${mheight}+150+150  ;#窗口左上角在150.150处。
 
     #标题：皮皮笔记
     set tstr "\xe7\x9a\xae\xe7\x9a\xae\xe7\xac\x94\xe8\xae\xb0";
-    set tt [encoding convertfrom  utf-8 $tstr]
-    wm title . "$tt pipi.notes windows ver 1.0"
 
+# "皮皮笔记 测试版 ".encode()
+  #  set tstr "\xe7\x9a\xae\xe7\x9a\xae\xe7\xac\x94\xe8\xae\xb0 \xe6\xb5\x8b\xe8\xaf\x95\xe7\x89\x88 ";
+  
+  set tt [encoding convertfrom  utf-8 $tstr]
+  wm title . "$tt pipi.notes windows ver 1.010"
 
 
     set upframe [frame .f -width $mwidth -height $mheight ;]
@@ -46,30 +49,15 @@ proc initial {} {
     set upscrollbar [scrollbar .f.scroll -command { .f.t yview }]
 
 
-    grid $uptitle
-    grid $upttext  -sticky nsew
-    grid $upscrollbar -row 1 -column 3 -sticky nsew
-
-
     set bottomFrame [frame .f1]
 
-    set bottoml [label .f1.l -text "Entry:" ]
+    set bottoml [label .f1.l -text "Enter:" ]
+
+    set bottome [entry .f1.e -width 80 ]
+
+    set bottomBPress [button .f1.b -text "Press" -command show]
 
 
-    set bottome [entry .f1.e -width 80]
-
-    set bottomBPress [button .f1.b -text "Press" -command entryneirong]
-
-
-    grid $bottoml  -column 0 -row 0 -sticky w -columnspan 1
-    grid $bottome -column 1 -row 0 -sticky nsew -columnspan 1
-    grid $bottomBPress -column 12 -row 0 -sticky e  -columnspan 1
-
-    #grid configure $bottome -columnspan 10
-
-
-    grid $upframe -row 0   -sticky nsew
-    grid $bottomFrame -row 1   -sticky nsew
 
 }
 
@@ -87,7 +75,11 @@ event add <<Enter1>> <Return>
 bind .f1.e <Control-i> {
 
     set name  [string trim [.f1.e get]	]  ;#id、笔记
-    #id、不用输入
+    #id是自动插入数据库的，不用输入。
+	
+	#把名字里面的回车换行符去掉，
+	set name [string map {"\r\n"  "" }  $name]
+	
     set neirong [.f.t get 1.0 end];#内容
 
     set name [string trim $name]
@@ -117,6 +109,9 @@ bind .f1.e <Control-f> {
 proc search {} {
 	#获取搜索内容
     set et [string trim [.f1.e get]	]
+	
+	#把名字里面的回车换行符去掉，
+	set et [string map {"\r\n"  "" }  $et]
 	
     if {$et != ""}  {
 	
@@ -206,20 +201,22 @@ bind .f.t  <Control-t> {
 	#用select 得到这个记录，
 	set jilu [db eval "select * from notes where id=[expr $r+1]"]
 
-	#，。！？到utf-8
+	#，。！？；到utf-8
 	set d1 "\xef\xbc\x8c"
 	set d2 "\xe3\x80\x82"
 	set d3 "\xef\xbc\x81"
 	set d4 "\xef\xbc\x9f"
+	set d5 "\xef\xbc\x9b"
 	
 	set tt1 [encoding convertfrom  utf-8 $d1]
 	set tt2 [encoding convertfrom  utf-8 $d2]
 	set tt3 [encoding convertfrom  utf-8 $d3]
 	set tt4 [encoding convertfrom  utf-8 $d4]
+	set tt5 [encoding convertfrom  utf-8 $d5]
 	
 	foreach {id name neirong} $jilu {
 	    
-	    set  m [split $neirong "$tt1$tt2$tt3$tt4 "]
+	    set  m [split $neirong "$tt1$tt2$tt3$tt4$tt5 "]
 	    #set  m [split $neirong "\r\n"];#以行来split拆分。
 	    #.f.t insert end "\n$m\nrand:[llength $m ]";
 	    
@@ -263,7 +260,12 @@ bind .f1.e  <Control-u> {
     
     #获取要更新的记录的id号(整数)	，	
     set et [string trim [.f1.e get]	]
+	
+	#把名字里面的回车换行符去掉，
+	set et [string map {"\r\n"  "" }  $et]
+	
     set ett [string is  digit $et]
+	
     if {$et != "" && $ett == 1 && $sums>=$et}  {
 	update0
     }
@@ -279,42 +281,48 @@ proc update0 {} {
     
     frame .tplupdate.f
     
-    label .tplupdate.f.ltext 
+    label .tplupdate.f.ltitle 
     
-    text .tplupdate.f.t -width 35 -height 10 -bg grey70 -font textfont  -undo true -yscrollcommand { .tplupdate.f.scroll set } 
+    text .tplupdate.f.t -width 40 -height 12 -bg grey70 -font textfont  -undo true -yscrollcommand { .tplupdate.f.scroll set } 
     scrollbar .tplupdate.f.scroll -command { .tplupdate.f.t yview }
 
 	frame .tplupdate.f1
-    label .tplupdate.f1.lId -text "id:"  
+    label .tplupdate.f1.l -text "id:"  
     entry .tplupdate.f1.e -width 60 
 
     button .tplupdate.f1.b -text "Update" -command {
 	update1
     }
 
+	#更新界面
+	set xsw [winfo screenwidth .]
+    set xsh [winfo screenheight .]
     
-    grid .tplupdate.f
-    grid .tplupdate.f.ltext 
-    
-    grid .tplupdate.f.t -column 0 -row 1 -sticky nsew -columnspan 3
-    grid .tplupdate.f.scroll  -column 3 -row 1 -sticky nsew
-    
-	grid .tplupdate.f1
-    grid .tplupdate.f1.lId -column 0 -row 2 -sticky w 
-    grid .tplupdate.f1.e -column 1 -row 2 -sticky nswe 
-    grid .tplupdate.f1.b -column 2 -row 2 -sticky nswe
-    
+    set mwidth [expr int($xsw*0.7)]
+    set mheight [expr  int($xsh*0.7)]
+	
+	wm geometry .tplupdate  ${mwidth}x${mheight}+150+150  ;
+	wm minsize .tplupdate 500 120 
+	
+	#改变尺寸
+	bind .tplupdate <Configure> {	
+		ResizeJiemian .tplupdate
+	}
+	
     #获取要更新的记录的id号(整数)	，
     set et [string trim [.f1.e get]	]
     
+	#把名字里面的回车换行符去掉，
+	set et [string map {"\r\n"  "" }  $et]
+	
     set x [db eval "select * from notes where id=$et"	]
 
 
     foreach  {id name neirong } $x {
-	.tplupdate.f.ltext configure -text "ID:$id\t$name"
+	.tplupdate.f.ltitle configure -text "ID:$id\t$name"
 	.tplupdate.f.t insert end "$neirong"
 	.tplupdate.f1.e insert 0 "$name"
-	.tplupdate.f1.lId configure -text "$id"
+	.tplupdate.f1.l configure -text "$id"
 
     }	
 }
@@ -322,11 +330,14 @@ proc update0 {} {
 #保存更新内容
 proc update1 {} {
     #获取要更新的记录的id号(整数)	，
-    set id [string trim [.tplupdate.f1.lId cget -text]	]
+    set id [string trim [.tplupdate.f1.l cget -text]	]
     
     #获取笔记名
     
     set name [.tplupdate.f1.e get]
+	
+	#把名字里面的回车换行符去掉，
+	set name [string map {"\r\n"  "" }  $name]
 
     #获取笔记的内容
     set neirong [.tplupdate.f.t get 1.0 end ]
@@ -334,14 +345,81 @@ proc update1 {} {
 	#把单引号修改为\'，因为sql中单引号有特殊用途，不改会错。
 	set name [string map {'  '' }  $name]
 	set neirong [string map {'  '' }  $neirong]
+	
+	set xsw [winfo screenwidth .]
+    set xsh [winfo screenheight .]
+    
+    set mwidth [expr int($xsw*0.7)]
+    set mheight [expr  int($xsh*0.7)]
 
     toplevel .t1
- 
+ 	wm title .t1 "Update Ok."
+	wm maxsize .t1 $mwidth $mheight
+	wm maxsize .t1 $mwidth $mheight
+	wm geometry .t1  ${mwidth}x${mheight}+150+150  ;
+	
 	text .t1.t -width 43 -height 15 -bg grey70 -font textfont
-    grid .t1.t
-    .t1.t insert end  "\t id:$id\n $name\n $neirong\n"
-    
+    place .t1.t -relx 0.001 -y 0.001 -width  [expr $mwidth-1] -height [expr $mheight-1]
+    .t1.t insert end  "\t id:$id\nname:$name\nneirong:\n $neirong\n"
+
    db eval "update notes set  notesname='$name',notesneirong='$neirong' where id=$id"
+}
+
+
+
+
+
+#显示.f1.e中的字符串
+proc show {} {
+
+	#获取输入框.f1.e中的字符串
+	set et  [string trim [.f1.e get]	]
+
+	#把名字里面的回车换行符去掉，
+	set et [string map {"\r\n"  "" }  $et]
+	
+	if {$et != ""}  {
+		
+		#立即显示字符串
+		.f.t insert end "\n$et "    
+		.f1.e delete 0 end		
+	}
+}
+
+#动态修改程序大小
+bind . <Configure> {
+	ResizeJiemian .
+}
+
+#动态改变窗口
+proc ResizeJiemian {Window} {
+#当前窗口尺寸
+	set hei [winfo  height $Window]
+	set wid [winfo width $Window]
+	if {$Window == "."} {
+		set Window [string map { . ""} $Window]
+	}
+	set fontsize [font actual textfont -size]
+
+	#label尺寸
+	set labelheight 30
+	set labelwidth 100
+	
+	#scroll
+	set scrollwidth 25
+	
+	#主界面
+    place $Window.f  -x 1 -y 1 -width [expr $wid-1] -height [expr $hei-$labelheight-1]
+    place $Window.f1  -x 1 -y [expr $hei-$labelheight-5] -width [expr $wid-1] -height [expr $hei-$labelheight-1]    
+	
+	place $Window.f.ltitle -relx 0.001 -rely 0.001 -width  [expr $wid-2] -height $labelheight
+    place $Window.f.t   -relx 0.001 -y [expr $labelheight+2] -width  [expr $wid-$scrollwidth-4] -height [expr $hei-$labelheight*2-4]
+    place $Window.f.scroll  -x [expr $wid-$scrollwidth-1] -y [expr $labelheight+2] -width  $scrollwidth -height [expr $hei-$labelheight*2-1]
+
+    place $Window.f1.l  -x 1 -rely 0.001 -width $labelwidth -height $labelheight 
+    place $Window.f1.e -x [expr $labelwidth+1] -rely 0.001 -width [expr $wid-$labelwidth*2-5] -height $labelheight
+    place $Window.f1.b -x [expr $wid-$labelwidth-2] -rely 0.001 -width $labelwidth -height  $labelheight
+
 }
 
 #程序功能：
@@ -349,21 +427,7 @@ proc update1 {} {
 #Control-f 搜索笔记，
 #Control-i 添加笔记到数据库
 #Control-u 更新数据库中的内容。
-
 #所需程序
 #两个
 #一、notes.tcl ，tcl语言程序
 #二、notes.db ，slited3数据库
-
-#显示.f1.e中的字符串
-proc show {} {
-
-	#获取输入框.f1.e中的字符串
-	set et  [string trim [.f1.e get]	]
-	
-	if {$et != ""}  {
-		#立即显示字符串
-		.f.t insert end "\n$et "    
-		.f1.e delete 0 end		
-	}
-}
--- shici.tcl
+++ shici.tcl
@@ -22,17 +22,17 @@ proc initial {} {
     set xsw [winfo screenwidth .]
     set xsh [winfo screenheight .]
     
-    set mwidth [expr int($xsw*0.8)]
-    set mheight [expr  int($xsh*0.8)]
+    set mwidth [expr int($xsw*0.7)]
+    set mheight [expr  int($xsh*0.7)]
     
-    wm maxsize . $mwidth $mheight
-    wm minsize .  500 300
-    wm geometry . +150+150  ;#窗口左上角在150.150处。
+
+    wm minsize .  500 120
+    wm geometry . ${mwidth}x${mheight}+150+150  ;#窗口左上角在150.150处。
 
     #标题：皮皮诗词
     set tstr "\xe7\x9a\xae\xe7\x9a\xae\xe8\xaf\x97\xe8\xaf\x8d";
     set tt [encoding convertfrom  utf-8 $tstr]
-    wm title . "$tt pipi.shici windows ver 1.0"
+    wm title . "$tt pipi.shici windows ver 1.010"
 
 
 
@@ -46,31 +46,14 @@ proc initial {} {
 
     set upscrollbar [scrollbar .f.scroll -command { .f.t yview }]
 
-
-    grid $uptitle
-    grid $upttext  -sticky nsew
-    grid $upscrollbar -row 1 -column 3 -sticky nsew
-
-
     set bottomFrame [frame .f1]
 
-    set bottoml [label .f1.l -text "Entry:" ]
+    set bottoml [label .f1.l -text "Enter:" ]
 
 
     set bottome [entry .f1.e -width 80]
 
-    set bottomBPress [button .f1.b -text "Press" -command entryneirong]
-
-
-    grid $bottoml  -column 0 -row 0 -sticky w -columnspan 1
-    grid $bottome -column 1 -row 0 -sticky nsew -columnspan 1
-    grid $bottomBPress -column 12 -row 0 -sticky e  -columnspan 1
-
-    #grid configure $bottome -columnspan 10
-
-
-    grid $upframe -row 0   -sticky nsew
-    grid $bottomFrame -row 1   -sticky nsew
+    set bottomBPress [button .f1.b -text "Press" -command show]
 
 }
 
@@ -87,7 +70,11 @@ event add <<Enter1>> <Return>
 bind .f1.e <Control-i> {
 
     set name  [string trim [.f1.e get]	]  ;#id、诗词名称、朝代、作者
-    #id、不用输入
+    #id是自动插入数据库的，不用输入
+	
+	#把名字里面的回车换行符去掉，
+	set name [string map {"\r\n"  "" }  $name]
+	
     set neirong [.f.t get 1.0 end];#内容
 
     set name [string trim $name]
@@ -117,6 +104,9 @@ bind .f1.e <Control-f> {
 proc search {} {
 	#获取搜索内容
     set et [string trim [.f1.e get]	]
+	
+	#把名字里面的回车换行符去掉，
+	set et [string map {"\r\n"  "" }  $et]
 	
     if {$et != ""}  {
 	
@@ -205,20 +195,22 @@ bind .f.t  <Control-t> {
 	#用select 得到这个记录，
 	set jilu [db eval "select * from shici where id=[expr $r+1]"]
 
-	#，。！？到utf-8
+	#，。！？；到utf-8
 	set d1 "\xef\xbc\x8c"
 	set d2 "\xe3\x80\x82"
 	set d3 "\xef\xbc\x81"
 	set d4 "\xef\xbc\x9f"
+	set d5 "\xef\xbc\x9b"
 	
 	set tt1 [encoding convertfrom  utf-8 $d1]
 	set tt2 [encoding convertfrom  utf-8 $d2]
 	set tt3 [encoding convertfrom  utf-8 $d3]
 	set tt4 [encoding convertfrom  utf-8 $d4]
+	set tt5 [encoding convertfrom  utf-8 $d5]
 	
 	foreach {id name neirong} $jilu {
 	    
-	    set  m [split $neirong "$tt1$tt2$tt3$tt4 "]
+	    set  m [split $neirong "$tt1$tt2$tt3$tt4$tt5 "]
 	    #set  m [split $neirong "\r\n"];#以行来split拆分。
 	    #.f.t insert end "\n$m\nrand:[llength $m ]";
 	    
@@ -261,7 +253,12 @@ bind .f1.e  <Control-u> {
     
     #获取要更新的记录的id号(整数)	，	
     set et [string trim [.f1.e get]	]
+	
+	#把名字里面的回车换行符去掉，
+	set et [string map {"\r\n"  "" }  $et]
+	
     set ett [string is  digit $et]
+	
     if {$et != "" && $ett == 1 && $sums>=$et}  {
 	update0
     }
@@ -277,13 +274,13 @@ proc update0 {} {
     
     frame .tplupdate.f
     
-    label .tplupdate.f.ltext 
+    label .tplupdate.f.ltitle 
     
-    text .tplupdate.f.t -width 35 -height 10 -bg grey70 -font textfont  -undo true -yscrollcommand { .tplupdate.f.scroll set } 
+    text .tplupdate.f.t -width 40 -height 12 -bg grey70 -font textfont  -undo true -yscrollcommand { .tplupdate.f.scroll set } 
     scrollbar .tplupdate.f.scroll -command { .tplupdate.f.t yview }
 
 	frame .tplupdate.f1
-    label .tplupdate.f1.lId -text "id:"  
+    label .tplupdate.f1.l -text "id:"  
     entry .tplupdate.f1.e -width 60 
 
     button .tplupdate.f1.b -text "Update" -command {
@@ -291,28 +288,35 @@ proc update0 {} {
     }
 
     
-    grid .tplupdate.f
-    grid .tplupdate.f.ltext 
+	#更新界面
+	set xsw [winfo screenwidth .]
+    set xsh [winfo screenheight .]
     
-    grid .tplupdate.f.t -column 0 -row 1 -sticky nsew -columnspan 3
-    grid .tplupdate.f.scroll  -column 3 -row 1 -sticky nsew
-    
-	grid .tplupdate.f1
-    grid .tplupdate.f1.lId -column 0 -row 2 -sticky w 
-    grid .tplupdate.f1.e -column 1 -row 2 -sticky nswe  
-	grid .tplupdate.f1.b -column 2 -row 2 -sticky nswe
-    
+    set mwidth [expr int($xsw*0.7)]
+    set mheight [expr  int($xsh*0.7)]
+	
+	wm geometry .tplupdate  ${mwidth}x${mheight}+150+150  ;
+	wm minsize .tplupdate 500 120 
+	
+	#改变尺寸
+	bind .tplupdate <Configure> {	
+		ResizeJiemian .tplupdate
+	}
+	
     #获取要更新的记录的id号(整数)	，
     set et [string trim [.f1.e get]	]
     
+	#把名字里面的回车换行符去掉，
+	set et [string map {"\r\n"  "" }  $et]
+	
     set x [db eval "select * from shici where id=$et"	]
 
 
     foreach  {id name neirong } $x {
-	.tplupdate.f.ltext configure -text "ID:$id\t$name"
+	.tplupdate.f.ltitle configure -text "ID:$id\t$name"
 	.tplupdate.f.t insert end "$neirong"
 	.tplupdate.f1.e insert 0 "$name"
-	.tplupdate.f1.lId configure -text "$id"
+	.tplupdate.f1.l configure -text "$id"
 
     }	
 }
@@ -320,12 +324,15 @@ proc update0 {} {
 #保存更新内容
 proc update1 {} {
     #获取要更新的记录的id号(整数)	，
-    set id [string trim [.tplupdate.f1.lId cget -text]	]
+    set id [string trim [.tplupdate.f1.l cget -text]	]
     
     #获取诗名
     
     set name [.tplupdate.f1.e get]
 
+	#把名字里面的回车换行符去掉，
+	set name [string map {"\r\n"  "" }  $name]
+	
     #获取诗的内容
     set neirong [.tplupdate.f.t get 1.0 end ]
 
@@ -333,13 +340,78 @@ proc update1 {} {
 	set name [string map {'  '' }  $name]
 	set neirong [string map {'  '' }  $neirong]
 	
-    toplevel .t1
+	set xsw [winfo screenwidth .]
+    set xsh [winfo screenheight .]
+    
+    set mwidth [expr int($xsw*0.7)]
+    set mheight [expr  int($xsh*0.7)]
 
-    text .t1.t -width 43 -height 15 -bg grey70 -font textfont
-    grid .t1.t
-    .t1.t insert end  "\t id:$id\n $name\n $neirong\n"
+    toplevel .t1 
+	wm title .t1 "Update Ok."
+	wm maxsize .t1 $mwidth $mheight
+	wm maxsize .t1 $mwidth $mheight
+	wm geometry .t1  ${mwidth}x${mheight}+150+150  ;
+	
+	text .t1.t -width 43 -height 15 -bg grey70 -font textfont
+    place .t1.t -relx 0.001 -y 0.001 -width  [expr $mwidth-1] -height [expr $mheight-1]
+    .t1.t insert end  "\t id:$id\nname:$name\nneirong:\n $neirong\n"
 
   db eval "update shici set  shiciname='$name',shicineirong='$neirong' where id=$id"
+}
+
+
+
+#显示.f1.e中的字符串
+proc show {} {
+
+	#获取输入框.f1.e中的字符串
+	set et  [string trim [.f1.e get]	]
+	
+	#把名字里面的回车换行符去掉，
+	set et [string map {"\r\n"  "" }  $et]
+	
+	if {$et != ""}  {
+		#立即显示字符串
+		.f.t insert end "\n$et "    
+		.f1.e delete 0 end		
+	}
+}
+
+
+#动态修改程序大小
+bind . <Configure> {
+	ResizeJiemian .
+}
+
+#动态改变窗口
+proc ResizeJiemian {Window} {
+#当前窗口尺寸
+	set hei [winfo  height $Window]
+	set wid [winfo width $Window]
+	if {$Window == "."} {
+		set Window [string map { . ""} $Window]
+	}
+	set fontsize [font actual textfont -size]
+
+	#label尺寸
+	set labelheight 30
+	set labelwidth 100
+	
+	#scroll
+	set scrollwidth 25
+	
+	#主界面
+    place $Window.f  -x 1 -y 1 -width [expr $wid-1] -height [expr $hei-$labelheight-1]
+    place $Window.f1  -x 1 -y [expr $hei-$labelheight-5] -width [expr $wid-1] -height [expr $hei-$labelheight-1]    
+	
+	place $Window.f.ltitle -relx 0.001 -rely 0.001 -width  [expr $wid-2] -height $labelheight
+    place $Window.f.t   -relx 0.001 -y [expr $labelheight+2] -width  [expr $wid-$scrollwidth-4] -height [expr $hei-$labelheight*2-4]
+    place $Window.f.scroll  -x [expr $wid-$scrollwidth-1] -y [expr $labelheight+2] -width  $scrollwidth -height [expr $hei-$labelheight*2-1]
+
+    place $Window.f1.l  -x 1 -rely 0.001 -width $labelwidth -height $labelheight 
+    place $Window.f1.e -x [expr $labelwidth+1] -rely 0.001 -width [expr $wid-$labelwidth*2-5] -height $labelheight
+    place $Window.f1.b -x [expr $wid-$labelwidth-2] -rely 0.001 -width $labelwidth -height  $labelheight
+
 }
 
 #程序功能：
@@ -352,16 +424,3 @@ proc update1 {} {
 #两个
 #一、shici.tcl ，tcl语言程序
 #二、shici.db ，slited3数据库
-
-#显示.f1.e中的字符串
-proc show {} {
-
-	#获取输入框.f1.e中的字符串
-	set et  [string trim [.f1.e get]	]
-	
-	if {$et != ""}  {
-		#立即显示字符串
-		.f.t insert end "\n$et "    
-		.f1.e delete 0 end		
-	}
-}
